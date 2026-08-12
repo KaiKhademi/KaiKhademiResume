bf351a92cfd21f51b84169e1deacab4c9310d9ad
KaiKhademi <kiamehrkhademi2019@gmail.com>
2026-08-11 18:54:08 -0700

    Initial

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,20 @@
 import type { NextConfig } from "next";
 
+const repositoryName = "KaiKhademiResume";
+
 const nextConfig: NextConfig = {
     output: "export",
     trailingSlash: true,
+
+    basePath:
+        process.env.NODE_ENV === "production"
+            ? `/${repositoryName}`
+            : "",
+
+    assetPrefix:
+        process.env.NODE_ENV === "production"
+            ? `/${repositoryName}/`
+            : "",
 
     images: {
         unoptimized: true,
